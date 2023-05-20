0bf62d26dcc91e64b2d42cb489af1fcabbfed942
Eric Njanga <eric.njanga@gmail.com>
2023-05-20 00:19:11 -0400

    Removing all firebase instances

--- functions/src/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export function root(request: Request, response: Response) {
-
-    response.status(200).send('<h1>Express server is up and running!</h1>');
-
-}
